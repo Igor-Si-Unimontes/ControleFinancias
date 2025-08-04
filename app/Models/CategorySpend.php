2779731eeb034dbbcb78b876291a409530c8d54d
Igor Fernandes <joaobatista22072003@gmail.com>
2025-08-04 18:56:30 -0300

    FEAT: Adicionando relacionamento entre categoria e usuario

--- app/Models/CategorySpend.php
+++ app/Models/CategorySpend.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategorySpend extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'user_id'];
 
     public function spends()
     {
         return $this->hasMany(Spend::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Gain::class);
     }
+    public function categorySpends()
+    {
+        return $this->hasMany(CategorySpend::class);
+    }
 }
