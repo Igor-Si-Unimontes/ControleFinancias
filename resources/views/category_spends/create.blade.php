9f116d64d894c7aad29b4005fc3e013408713bab
Igor Fernandes <joaobatista22072003@gmail.com>
2025-08-04 18:57:26 -0300

    FEAT: Vinculando o crud com o relacionamento de categoria e usuario

--- resources/views/category_spends/create.blade.php
+++ resources/views/category_spends/create.blade.php
@@ -14,6 +14,7 @@
                     <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}"
                         required>
                 </div>
+                <input type="hidden" name="user_id" value="{{ auth()->id() }}">
             </div>
 
             <div class="d-flex justify-content-start gap-2 mt-3">
